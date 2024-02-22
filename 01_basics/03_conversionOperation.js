5e685d12614cab0b16142488c3b2f8ed3316ad4d
Mohammed-Mafaz <152594937+Mohammed-Mafaz@users.noreply.github.com>
2024-02-22 17:35:11 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+// let score=33
+// let score="33"     // number 
+// let score="33abc"  // NaN (but type is number)
+// let score=null;    // 0
+// let score=true;    // 1
+// let score=javascript; // NaN
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn=1
+
+let booleanIsLoggedInt=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedInt)
+
+// 1 => true; 0 => false
+// "" => false
+// "javascript" => true
+
+// let num=33
+
+// let stringNumber=String(num)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);  // string
+
+// ****************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2^3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2="world"
+
+let str3=str1+str2
+// console.log(str3)
+ 
+// console.log("1" +2); // 12
+// console.log(1 +"2"); // 12
+// console.log("1" +2+2);// 122
+// console.log(1 +2+"2");// 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100
+// gameCounter++; // postfix
+// ++gameCounter; // prefix
+// console.log(++gameCounter);
+
+
