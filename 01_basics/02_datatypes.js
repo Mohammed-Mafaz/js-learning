2304851f975f9056625694e2ef039c793f6ecaa4
Mohammed-Mafaz <152594937+Mohammed-Mafaz@users.noreply.github.com>
2024-02-22 16:55:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer js version
+
+// alert( 3 + 3) // we are using nodejs , not browse
+
+
+console.log(3+3)
+// code readability should be high
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (type - object)
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "javascript")
+console.log(typeof null) // object
+console.log(typeof undefined) // 
