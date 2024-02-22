c55e81ccdc404d789398dc3a4935d77d338be19f
Mohammed-Mafaz <152594937+Mohammed-Mafaz@users.noreply.github.com>
2024-02-22 17:52:26 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // true
+// console.log("02" > 1);// true
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true (null=>0)
+
+// Comparisons convert null to a number , treating it as 0.
+// that's why null >= 0 gives true
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+// console.log("2" == 2); // true
+// console.log("2" === 2); // strict check (it doesn't converts the datatypes)
