3492e6046af0c1a5a3f3b918f946336bfc168b2e
Mohammed-Mafaz <152594937+Mohammed-Mafaz@users.noreply.github.com>
2024-02-25 08:48:45 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+//var c =300
+let a = 300
+if(true){
+    a=10
+    const b=20
+    console.log("Inner a : ",a)
+}
+console.log(a);
+
+let i=100
+for(let i=0;i<array.length;i++){
+    const element=array[i];
+}
+console.log(i)
